ae08e84b4a41c9934fc17cae1a65c47a216c9944
Morok <97509540+Morok46@users.noreply.github.com>
2022-10-29 13:54:39 +0300

    Const 2

--- ExampleConst.go
+++ ExampleConst.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	const Pi float32 = 3.14
+	//const Pi float32 = 3.14
 
-	fmt.Println(Pi)
+	//const Pi = 3.14
+
+	const (
+		a = 1
+		b
+		c
+		d = 3
+		e
+	)
+
+	fmt.Println(a, b, c, d, e)
 }
