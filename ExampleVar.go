82585b57fc1b597ef84bf6fa9547f0dbbd9d5008
Morok <97509540+Morok46@users.noreply.github.com>
2022-10-29 18:21:15 +0300

    Go 01

--- ExampleVar.go
+++ ExampleVar.go
@@ -1,18 +1,18 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	//var Hello string
-	//Hello = "Hi all"
+// func main() {
+// 	//var Hello string
+// 	//Hello = "Hi all"
 
-	//var Hello string = "Hi all"
+// 	//var Hello string = "Hi all"
 
-	var (
-		Name string = "Serg"
-		Age  int    = 22
-	)
-	//Hello := "Hay u"
-	fmt.Println(Name)
-	fmt.Println(Age)
-}
+// 	var (
+// 		Name string = "Serg"
+// 		Age  int    = 22
+// 	)
+// 	//Hello := "Hay u"
+// 	fmt.Println(Name)
+// 	fmt.Println(Age)
+// }
