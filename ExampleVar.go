c21656fd103b669bd8d0ac4f2f05b9565a7d9fc6
Morok <97509540+Morok46@users.noreply.github.com>
2022-10-29 12:28:49 +0300

    Var 3

--- ExampleVar.go
+++ ExampleVar.go
@@ -5,6 +5,14 @@ import "fmt"
 func main() {
 	//var Hello string
 	//Hello = "Hi all"
-	var Hello string = "Hi all"
-	fmt.Println(Hello)
+
+	//var Hello string = "Hi all"
+
+	var (
+		Name string = "Serg"
+		Age  int    = 22
+	)
+	//Hello := "Hay u"
+	fmt.Println(Name)
+	fmt.Println(Age)
 }
