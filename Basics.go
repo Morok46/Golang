73d30e2dd9d52d4cb20cf2277984bcb5c431a81e
Morok46 <kushnir_m@list.ru>
2022-11-03 09:46:00 +0300

    Basics 03

--- Basics.go
+++ Basics.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
-func add(x int, y int) int {
+func add(x, y int) int {
 	return x + y
 }
+func swap(x, y string) (string, string) {
+	return x, y
+}
+
+// func main() {
+// 	fmt.Println(add(62, 13))
+// }
 
 func main() {
+	a, b := swap("Hello", "world")
+	fmt.Println(a+b, b)
+
 	fmt.Println(add(62, 13))
 }
